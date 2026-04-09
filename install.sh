801f159bc5937539e3ee93337909d194da356788
johannfleitas <joivflca2003@gmail.com>
2026-04-09 20:27:08 -0300

    Actualización de configuraciones 2026-04-09 20:27:08

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ PACMAN_APPS_UNINSTALL=(wofi)
 PACMAN_APPS_INSTALL=(alacritty waybar starship rofi hyprpaper obsidian 
                     pavucontrol polkit-kde-agent power-profiles-daemon 
                     brightnessctl unrar unzip firefox network-manager-applet
-                    debugedit fakeroot)
+                    debugedit fakeroot base-devel git)
 
 YAY_APPS=(helium-browser-bin eww)
 
